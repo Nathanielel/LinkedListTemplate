d3df127d193663bd0154a404c5e971f08dbb300d
nathanielel <nathaniel.e.larsen@gmail.com>
2020-09-19 11:57:52 -0700

    added special removal fns

--- List.cpp
+++ List.cpp
@@ -3,170 +3,257 @@
    List.cpp - Linked List class template Implementaion File
 */
 
-#include <iostream>
 #include "List.h"
 
+#include <iostream>
+
 /*
-*  Node class to contain data for linked list
-*/
+ *  Node class to contain data for linked list
+ */
 
 template <class T>
-class LinkNode{
-  friend class List<T>; // list can modify node members
-public:
-  LinkNode(){next = 0; prev = 0; data = 0;} // default constructor
-  LinkNode(T* val); // constructor from data value
-  LinkNode(LinkNode<T>& toCopy); // copy constructor
-  ~LinkNode(){if(data){delete data; data = 0;}} // destructor
-private:
-  T* data;
-  LinkNode* next;
-  LinkNode* prev;
+class LinkNode {
+  friend class List<T>;  // list can modify node members
+ public:
+  LinkNode() {
+    next = 0;
+    prev = 0;
+    data = 0;
+  }                               // default constructor
+  LinkNode(T *val);               // constructor from data value
+  LinkNode(LinkNode<T> &toCopy);  // copy constructor
+  ~LinkNode() {
+    if (data) {
+      delete data;
+      data = 0;
+    }
+  }  // destructor
+ private:
+  T *data;
+  LinkNode *next;
+  LinkNode *prev;
 };
 
 template <class T>
-LinkNode<T>::LinkNode(T* val){
-  data = new T(); //allocate memory and point data to it
-  *data = *val; //dereference and default memberwise copy or op= 
-  next = 0; prev = 0; 
+LinkNode<T>::LinkNode(T *val) {
+  data = new T();  // allocate memory and point data to it
+  *data = *val;    // dereference and default memberwise copy or op=
+  next = 0;
+  prev = 0;
 }
 
 template <class T>
-LinkNode<T>::LinkNode(LinkNode<T>& toCopy) {
+LinkNode<T>::LinkNode(LinkNode<T> &toCopy) {
   data = new T();
   *data = toCopy->data;
   next = prev = 0;
 }
 
 template <class T>
-List<T>::~List(){
-  LinkNode<T>* temp = head;
-  while(head){ // list is not empty
-    temp = head; // keep reference to head
-    head = head->next; // move head to next node
-    delete temp; // calls linknode destructor
+List<T>::~List() {
+  LinkNode<T> *temp = head;
+  while (head) {        // list is not empty
+    temp = head;        // keep reference to head
+    head = head->next;  // move head to next node
+    delete temp;        // calls linknode destructor
   }
 }
 
 template <class T>
-void List<T>::push_front(T* new_data){
-  ++size; 
-  LinkNode<T>* temp = new LinkNode<T>(new_data); //create a new linknode to prepend to list
-  if(!head){ //empty list case
+void List<T>::push_front(T *new_data) {
+  ++size;
+  LinkNode<T> *temp =
+      new LinkNode<T>(new_data);  // create a new linknode to prepend to list
+  if (!head) {                    // empty list case
     head = tail = temp;
     return;
   }
-  //otherwise, attach and point head to new node
-  temp->next = head; head->prev = temp;
-  head = temp;                                                                       }
+  // otherwise, attach and point head to new node
+  temp->next = head;
+  head->prev = temp;
+  head = temp;
+}
 
 template <class T>
-void List<T>::push_back(T* new_data){
+void List<T>::push_back(T *new_data) {
   ++size;
-  LinkNode<T>* temp = new LinkNode<T>(new_data);
-  if(!tail) { //empty list
+  LinkNode<T> *temp = new LinkNode<T>(new_data);
+  if (!tail) {  // empty list
     head = tail = temp;
     return;
   }
-  //otherwise, attach and point tail to new node
-  temp->prev = tail; tail->next = temp;
+  // otherwise, attach and point tail to new node
+  temp->prev = tail;
+  tail->next = temp;
   tail = temp;
 }
 
 template <class T>
-T* List<T>::pop_front(){
-  if (!head) return NULL; //empty list
-  --size; 
-  T* result = head->data; //point result to head's data - return value
-  head->data = 0; //release head node's data pointer
+T *List<T>::pop_front() {
+  if (!head) return NULL;  // empty list
+  --size;
+  T *result = head->data;  // point result to head's data - return value
+  head->data = 0;          // release head node's data pointer
 
-  if (head == tail){ //one element list
+  if (head == tail) {  // one element list
     delete head;
     head = tail = 0;
     return result;
   }
 
-  LinkNode<T>* temp = head; // get reference to current head node
+  LinkNode<T> *temp = head;  // get reference to current head node
 
-  //detach head and point head to next element
-  head->next->prev = 0; head = head->next; 
+  // detach head and point head to next element
+  head->next->prev = 0;
+  head = head->next;
   delete temp;
   return result;
 }
 
 template <class T>
-T* List<T>::pop_back(){
-  T* result = 0;
-  if (!tail) return 0; //empty list
+T *List<T>::pop_back() {
+  T *result = 0;
+  if (!tail) return 0;  // empty list
   --size;
   result = tail->data;
   tail->data = 0;
-  if (head == tail){
+  if (head == tail) {
     delete tail;
     head = tail = 0;
     return result;
   }
-  LinkNode<T>* temp = tail; // get reference to current tail node
+  LinkNode<T> *temp = tail;  // get reference to current tail node
 
-  //detach tail and point tail to prior element
-  tail->prev->next = 0; tail = tail->prev;
+  // detach tail and point tail to prior element
+  tail->prev->next = 0;
+  tail = tail->prev;
   delete temp;
   return result;
 }
 
 template <class T>
-void List<T>::insert_at(size_t index, T* val){
-  if (index > size || index < 0) return; //out of range
-  if (index == size){ //end of list
-    push_back(val); return;
+void List<T>::insert_at(size_t index, T *val) {
+  if (index > size || index < 0) return;  // out of range
+  if (index == size) {                    // end of list
+    push_back(val);
+    return;
   }
-  if (index == 0){ //start of list
-    push_front(val); return;
+  if (index == 0) {  // start of list
+    push_front(val);
+    return;
   }
   ++size;
-  LinkNode<T>* link = head; 
-  //traversing starting @ index 0
-  for(unsigned i = 0; i < index; ++i){
+  LinkNode<T> *link = head;
+  // traversing starting @ index 0
+  for (unsigned i = 0; i < index; ++i) {
     link = link->next;
   }
-  //link now points to node to follow new node to be inserted
-  LinkNode<T>* anchor = link->prev; // get reference to node to come before new element
-  LinkNode<T>* temp = new LinkNode<T>(val); // allocate memory for new element
-  anchor->next = temp; temp->prev = anchor; // attach new element to front of list
-  temp->next = link; link->prev = temp; //atttach the rest of list to new element
-
+  // link now points to node to follow new node to be inserted
+  LinkNode<T> *anchor =
+      link->prev;  // get reference to node to come before new element
+  LinkNode<T> *temp = new LinkNode<T>(val);  // allocate memory for new element
+  anchor->next = temp;
+  temp->prev = anchor;  // attach new element to front of list
+  temp->next = link;
+  link->prev = temp;  // atttach the rest of list to new element
 }
 
-//PRECONDITION: T type has stream insertion operation defined.
+// PRECONDITION: T type has stream insertion operation defined.
 template <class T>
-void List<T>::print(){
-  LinkNode<T>* temp = head;
-  while (temp){
+void List<T>::print() {
+  LinkNode<T> *temp = head;
+  while (temp) {
     std::cout << *(temp->data);
-    if (temp-> next)
-      std::cout << ": ";
+    if (temp->next) std::cout << ": ";
     temp = temp->next;
   }
   std::cout << std::endl;
 }
 
 template <class T>
-void List<T>::remove_all(T* val){
-
+void List<T>::remove_all(T *val) {
+  LinkNode<T> *temp = head, *follow = NULL;
+  while (temp) {
+    if (*(temp->data) == *val) {  // found node to delete
+      LinkNode<T> *trash = temp;
+      temp = temp->next;
+      // splice out trash
+      if (follow) follow->next = temp;
+      if (temp) temp->prev = follow;
+      delete trash;
+      --size;
+    } else {
+      follow = temp;
+      temp = temp->next;
+    }
+  }
 }
 
 template <class T>
-void List<T>::remove_first(T* val){
-
+void List<T>::remove_first(T *val) {
+  LinkNode<T> *temp = head, *follow = NULL;
+  while (temp) {
+    if (*(temp->data) == *val) {  // match found
+      if (!follow) {              // temp is head
+        erase_front();
+        return;
+      }
+      follow->next = temp->next;
+      if (temp->next)  // temp != tail
+        temp->next->prev = follow;
+      delete temp;
+      --size;
+      return;
+    }
+    follow = temp;
+    temp = temp->next;
+  }
 }
 
 template <class T>
-void List<T>::erase(size_t index){
+void List<T>::erase_front() {
+  T *trash = pop_front();
+  delete trash;
+  return;
+}
+
+template <class T>
+void List<T>::erase_back() {
+  T *trash = pop_back();
+  delete trash;
+  return;
+}
+
+template <class T>
+void List<T>::erase(size_t index) {
+  // bounds check of index
   if (index >= size || index < 0) return;
-
+  // handle edge cases in O(1) time
+  if (index == size - 1) {
+    erase_back();
+    return;
+  }
+  if (index == 0) {
+    erase_front();
+    return;
+  }
+  // Node to erase is btwn 2 nodes, function slows to O(n)
+  LinkNode<T> *temp = head;
+  size_t at_index = 0;
+  while (at_index != index) {
+    temp = temp->next;
+    ++at_index;
+  }  // temp now == node to splice out
+  LinkNode<T> *before = temp->prev;
+  LinkNode<T> *after = temp->next;
+  before->next = after;
+  after->prev = before;
+  --size;
+  delete temp;
 }
 
-//since I moved the functionality to a cpp file, these template lines must be placed here to deal with undefined reference issues
-template class List<int>; 
+// since I moved the functionality to a cpp file, these template lines must be
+// placed here to deal with undefined reference issues
+template class List<int>;
 template class List<double>;
 template class List<std::string>;
--- List.h
+++ List.h
@@ -1,7 +1,7 @@
 /**
    Nathaniel Larsen - nlarsen - nathaniel.e.larsen@gmail.com
    List.h - Linked List class template Header File
-   Assignment 1 - Linked List Review
+   Support for T of type: int, double, string.
 */
 
 #ifndef LINKEDLIST_H
@@ -10,7 +10,7 @@
 #include <iostream>
 
 template <class T>
-class LinkNode; //forward declaration
+class LinkNode;  // forward declaration
 
 /*
   Linked List Template class - doubly linked list generic class
@@ -18,21 +18,27 @@ class LinkNode; //forward declaration
 */
 
 template <class T>
-class List{
+class List {
  public:
-  List(){head = 0; tail = 0; size = 0;}
-  //List(List&); // copy constructor --TODO
+  List() {
+    head = 0;
+    tail = 0;
+    size = 0;
+  }
+  // List(List&); // copy constructor --TODO
   ~List();
-  void push_front(T*); //prepend to list
-  void push_back(T*);  //appent to list
-  void insert_at(size_t, T*); //insert an element at given index 
-  T* pop_front(); //pop the data from the front link node off of the list
-  T* pop_back();  //pop the data from the back link node off of the list
-  void remove_all(T* val);
-  void remove_first(T* val);
-  void erase(size_t index);
-  void print();
-  size_t length(){ return size; } 
+  void push_front(T*);         // prepend to list
+  void push_back(T*);          // append to list
+  void insert_at(size_t, T*);  // insert an element at given index
+  T* pop_front();  // pop the data from the front link node off of the list
+  T* pop_back();   // pop the data from the back link node off of the list
+  void remove_all(T* val);    // remove every occurrence of val from the list
+  void remove_first(T* val);  // remove first occurrence of val from the list
+  void erase(size_t index);   // erase the element at given index
+  void erase_front();         // erase the first element of the list
+  void erase_back();          // erase the last element of the list
+  void print();  // print the list. PRE: T stream insertion operator is defined
+  size_t length() { return size; }
  private:
   LinkNode<T>* head;
   LinkNode<T>* tail;
